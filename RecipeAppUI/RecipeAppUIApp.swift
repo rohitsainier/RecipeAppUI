66af439e66106d2685c7f6afb86d52a344621001
rohitsainier <rohitsainier@gmail.com>
2022-05-03 13:02:58 +0530

    Initial Commit

---
+++ RecipeAppUI/RecipeAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppUIApp.swift
+//  RecipeAppUI
+//
+//  Created by Rohit Saini on 03/05/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
